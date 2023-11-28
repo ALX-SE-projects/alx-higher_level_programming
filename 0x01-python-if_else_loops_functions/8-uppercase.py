be48da774da6c27d59ced2f881b0be0c01ebfb95
ibrahemesam <iimosa7777@gmail.com>
2023-11-29 00:59:44 +0200

    blabla bla

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    lower_range = range(ord('a'), ord('z') + 1)
+    for idx, i in enumerate(str):
+        if i in lower_range:
+            str[idx] = ord('A') + (ord(i) - ord('a'))
+    return str
