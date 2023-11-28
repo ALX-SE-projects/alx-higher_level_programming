f0faa06dab4bdb03102d0875a9844b1997fb8cb2
ibrahemesam <iimosa7777@gmail.com>
2023-11-29 01:05:04 +0200

    blabla bla

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     lower_range = range(ord('a'), ord('z') + 1)
+    str = list(str)
     for idx, i in enumerate(str):
-        if i in lower_range:
-            str[idx] = ord('A') + (ord(i) - ord('a'))
-    print("{}".format(str))
+        o = ord(i)
+        if o in lower_range:
+            str[idx] = chr(ord('A') + (o - ord('a')))
+    print("{}".format("".join(str)))
