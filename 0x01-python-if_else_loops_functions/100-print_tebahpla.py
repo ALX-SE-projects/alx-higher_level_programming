baf8d3d6306e287f1a882f43201cf95bdcd59a48
ibrahemesam <iimosa7777@gmail.com>
2023-11-29 01:23:02 +0200

    blabla bla

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+is_up = -1
+s = ''
+for i in list(range(ord('a'), ord('z') + 1))[::-1]:
+    if is_up > 0:
+        s += chr(ord('A') + (i - ord('a')))
+    else:
+        s += chr(i)
+    is_up = -is_up
+print(s, end='')
