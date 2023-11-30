7cff1928a2249ce31b1abbc9e21ff3c41506cd6d
ibrahemesam <iimosa7777@gmail.com>
2023-11-30 20:26:43 +0200

    blabla bla

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     from sys import argv
     argv = argv[1:]
     _len = len(argv)
-    print(f"{_len} argument", end='')
+    print(f"{_len} arguments", end='')
     if _len == 0:
         print('.')
     else:
