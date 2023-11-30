76018fe2e0d213926721e816f8f7b7704e9f35f5
ibrahemesam <iimosa7777@gmail.com>
2023-11-30 20:24:09 +0200

    blabla bla

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+    argv = argv[1:]
+    _len = len(argv)
+    print(f"{_len} argument", end='')
+    if _len == 0:
+        print('.')
+    else:
+        print(':')
+        for idx, arg in enumerate(argv, start=1):
+            print("{}: {}".format(idx, arg))
