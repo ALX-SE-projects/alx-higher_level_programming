978eaa797455daac013a5dd838a9fc281ab472f0
ibrahemesam <iimosa7777@gmail.com>
2023-11-30 20:29:07 +0200

    blabla bla

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,12 @@ if __name__ == '__main__':
     from sys import argv
     argv = argv[1:]
     _len = len(argv)
-    print(f"{_len} arguments", end='')
+    print(f"{_len} argument", end='')
     if _len == 0:
         print('.')
     else:
+        if _len == 1:
+            print("s", end='')
         print(':')
         for idx, arg in enumerate(argv, start=1):
             print("{}: {}".format(idx, arg))
