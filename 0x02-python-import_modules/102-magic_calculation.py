41356b58f728c33fe45e290df0e1cb767ed52c60
ibrahemesam <iimosa7777@gmail.com>
2023-11-30 15:39:01 +0200

    blabla bla

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    if a, b == 10, 15:
+    if (a, b) == (10, 15):
         return -14
-    elif a, b == 23, 17:
+    elif (a, b) == (23, 17):
         return 40
