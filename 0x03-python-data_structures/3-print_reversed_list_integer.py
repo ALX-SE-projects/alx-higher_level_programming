cbfb347d528b103b5a5a20ee546d56e3a37a8a3d
ibrahemesam <iimosa7777@gmail.com>
2023-12-04 04:03:09 +0200

    blabla bla

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,3 +2,4 @@
 def print_list_integer(my_list=[]):
     for i in my_list[::-1]:
         print(i)
+        # print("{:d}".format(i))
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,3 +2,4 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         print(' '.join(row))
+        # print("{:d}".format(i))
