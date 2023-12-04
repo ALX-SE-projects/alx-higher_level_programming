28f5c635f56a5709632d84ec4c31d34d3ad3f33f
ibrahemesam <iimosa7777@gmail.com>
2023-12-04 04:18:37 +0200

    blabla bla

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence:
-        _len = lenn(sentence)
+        _len = len(sentence)
         first_letter = sentence[0]
     else:
         _len = 0
