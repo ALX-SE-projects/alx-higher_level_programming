d0af16479f6a5eb0b77db3c69ad1ba1ca99e9a0f
ibrahemesam <iimosa7777@gmail.com>
2023-12-19 02:09:17 +0200

    blabla bla

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,10 +3,9 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
 
 
 if __name__ == '__main__':
     safe_print_integer('s')
-
