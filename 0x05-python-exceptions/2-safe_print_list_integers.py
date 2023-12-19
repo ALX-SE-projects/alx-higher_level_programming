757a76d3877de2a410aaf18dc5c57d7576800327
ibrahemesam <iimosa7777@gmail.com>
2023-12-19 02:14:26 +0200

    blabla bla

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    print("")
+    for j in range(x):
+        try:
+            print("{:d}".format(my_list[j]))
+            i += 1
+        except IndexError as e:
+            raise e
+    return i
+
+
+if __name__ == '__main__':
+    safe_print_integer('s')
