7310829f3d55d3344183e32bc510b45e687774a2
ibrahemesam <iimosa7777@gmail.com>
2023-12-20 02:46:32 +0200

    blabla bla

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+"""Defines a class Square.
+
+This module demonstrates documentation as specified by the `Google Python
+Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+with a section header and a colon followed by a block of indented text.
+
+Example:
+    Examples can be given using either the ``Example`` or ``Examples``
+    sections. Sections support any reStructuredText formatting, including
+    literal blocks::
+
+        $ python example_google.py
+
+Section breaks are created by resuming unindented text. Section breaks
+are also implicitly created anytime a new section starts.
+
+Attributes:
+    module_level_variable1 (int): Module level variables may be documented in
+        either the ``Attributes`` section of the module docstring, or in an
+        inline docstring immediately following the variable.
+
+        Either form is acceptable, but the two should not be mixed. Choose
+        one convention to document module level variables and be consistent
+        with it.
+
+Todo:
+    * For module TODOs
+    * You have to also use ``sphinx.ext.todo`` extension
+
+.. _Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+"""
 
 
 class Square:
@@ -7,6 +38,7 @@ class Square:
     Attributes:
         __size (int): size of a side of the square
     """
+
     def __init__(self, size=0):
         """initializes the square
         Args:
@@ -14,14 +46,11 @@ class Square:
         Returns:
             None
         """
-        self.size = size
-
-    def area(self):
-        """calculates the square's area
-        Returns:
-            The area of the square
-        """
-        return (self.__size) ** 2
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
 
     @property
     def size(self):
@@ -31,7 +60,7 @@ class Square:
         """
         return self.__size
 
-    @size.setter
+    @x.setter
     def size(self, value):
         """setter of __size
         Args:
@@ -41,8 +70,12 @@ class Square:
         """
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        else:
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """calculates the square's area
+        Returns:
+            The area of the square
+        """
+        return self.__size ** 2
